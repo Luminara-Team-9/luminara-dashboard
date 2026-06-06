79e32d70775d5ccd8441879449e483ce7bc81b28
MAY1KNU <may1724@knu.ac.kr>
2026-06-06 19:28:29 +0900

    chore: perfectly sync active-staging with base-clone

--- 2_digital_twins/active-staging/src/shared/analytics/SwetrixTracker.tsx
+++ 2_digital_twins/active-staging/src/shared/analytics/SwetrixTracker.tsx
@@ -1,19 +1,23 @@
 'use client';
 
 import { useEffect } from 'react';
+// import { usePathname } from 'next/navigation';
 import { init, trackViews } from 'swetrix';
 
 let initialized = false;
 
 export function SwetrixTracker() {
+  // const pathname = usePathname();
+
   useEffect(() => {
     if (!initialized) {
-      init('6MZNYXghl1v8', {
+      init('n9FlE09mPFlv', {
         apiURL: 'http://155.230.135.209:5005/log',
       });
       trackViews();
       initialized = true;
     }
   }, []);
+
   return null;
 }
